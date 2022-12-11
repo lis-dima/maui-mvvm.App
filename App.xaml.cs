9d5075a86de4aec0049a8885c56e23126005701f
lis-dima <lisiev.dmytro@gmail.com>
2022-12-11 16:50:02 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace maui_mvvm;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace maui_mvvm;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
